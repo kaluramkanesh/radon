ed1ce656e18134be0c7f3bc54501875a8747f074
Kaluram <kaluramkanesh123@gmail.com>
2022-06-03 08:43:27 +0530

    chunk tail union assignment

---
+++ src/hello/hello.js
@@ -0,0 +1,19 @@
+const x = require("underscore")
+function lodashChunk() {
+    const m = x.chunk(["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"], 4)
+    console.log(m)
+}
+function loDashTail() {
+const t=x.tail([1,3,5,7,9,11,13,15,17,19])
+console.log(t)
+}
+function fromPairs(){
+     const u=x.union([["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]
+    
+ ])
+// const u=x.union([2,3,4,5,6],[2,5,8,0],[1,3,6])
+console.log(u)
+}
+module.exports.lodashChunk = lodashChunk
+module.exports.loDashTail=loDashTail
+module.exports.lodashUnion=fromPairs
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,32 +1,69 @@
 const express = require('express');
-const underscore=require("underscore")
+// const underscore=require("underscore")
+const helloChunk = require("../hello/hello")
 const router = express.Router();
-router.get('/test-me', function (req, res) {
-    let firstElement=underscore.first(["Kaluram","Seema","Rahul","Manish"])
-    console.log("The first receved from underScore",firstElement)
-    console.log(underscore.last([23,4,5,6,7,8,91]))
-    res.send('My first ever api! Hello kaluram,How are you dear?')
+router.get('/chunk', function (req, res) {
+    helloChunk.lodashChunk()
+    helloChunk.loDashTail()
+    helloChunk.lodashUnion()
+    res.send("Done")
 });
-router.get("/hello",function(req,res){
-    res.send("Hello ram this is hello api's")
-})
-router.get("/candidates",function(req,res){
-    console.log("query paramets for this request are ",JSON.stringify(req.query))
-    let name=req.query.name
-    let state=req.query.state
-    let district = req.query.district
-    console.log("name=>",name)
-    console.log("Condidates state ",state)
-    console.log("candedates distric ",district)
-    let candedates=["Kaluram","Rahul","Manish","Hemraj"]
-    res.send(candedates)
+
+//Create an API for GET /movies that returns a list of movies. Define an array of movies
+// in your code and return the value in response.
+router.get("/GET/movies", function (req, res) {
+    const movies = ["Rang de basanti", "The shining", "RRR", "KGF", "Yamla pagla diwana", "Krantivir"]
+    res.send(movies)
 })
 
-router.get("/candidates1/:candidatesname",function(req,res){
-    console.log("params paramets for this request are ",JSON.stringify(req.params))
-    console.log("candidates name is ",req.params.candidatesname)
-    res.send(req.params.candidatesname)
+//Create an API GET /movies/:indexNumber (For example GET /movies/1
+// is a valid request and it should return the movie in your array at index 
+//1). You can define an array of movies again in your api
+router.get("/GET/movies1/:indexNumber", function (req, res) {
+    const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    const s = req.params.indexNumber
+    console.log(s)
+    // for (let i = 0; i < movies.length - 1; i++) {
+    //     if (s === movies[i]) {
+    //         res.send(movies[i])
+    //     }
+    // }
+    //res.send(movies[req.params.indexNumber])
 })
 
+router.get("GET/movies2/:indexNumber", function (req, res) {
+    const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    const s = req.params.indexNumber
+    for (let i = 0; i < movies.length - 1; i++) {
+        if (s === movies[i]) {
+            res.send(movies[i])
+        }
+        else {
+            res.send("use a valid index in an error message.")
+        }
+    }
+})
+router.get("GET/film", function (req, res) {
+    console.log(req)
+     f = [
+        {
+            'id': 1,
+            'name': 'The Shining'
+        },
+        {
+            'id': 2,
+            'name': 'Incendies'
+        },
+        {
+            'id': 3,
+            'name': 'Rang de Basanti'
+        },
+        {
+            'id': 4,
+            'name': 'Finding Nemo'
+        }
+    ]
+    // res.send(f)
+})
 module.exports = router;
 // adding this comment for no reason
