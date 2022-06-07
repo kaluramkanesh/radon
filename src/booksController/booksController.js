ec696133f491355ca2324c93c28514cfd7d20314
Kaluram <kaluramkanesh123@gmail.com>
2022-06-07 12:55:56 +0530

    some-changes-in-code

--- src/booksController/booksController.js
+++ src/booksController/booksController.js
@@ -1,23 +1,46 @@
 const bookeSechma = require("../bookSchema/bookSchema")
 
-const createNewBooks = async function (req, res) {
+const NewBooksEntry = async function (req, res) {
     let data = req.body
     let savaData = await bookeSechma.create(data)
     res.send(savaData)
 }
 
-const getBookPost = async function (req, res) {
-    let year = req.query.years
-    let allBooks = await bookeSechma.find({ year })
-    res.send(allBooks)
+const getBooksInYear = async function (req, res) {
+    // let year = req.query.years
+    // let allBooks = await bookeSechma.find({ year })
+    // res.send(allBooks)
+    let year=req.body
+    let getBook=await bookeSechma.find(year)
+    res.send(getBook)
 }
-const getnewBook = async function (req, res) {
-     //let getAllBooks=await bookeSechma.find().select({bookeName:1,authorName:1,_id:0 })
-   // let getAllBooks = await bookeSechma.find({$or:[{price:{ IndianPrice: {$or:["Rs. 100"]} }} ,  {price:{ IndianPrice: {$or:["Rs. 200"] } }},{ price: { IndianPrice: {$or:["Rs. 500"] }}} ]})
-     let getAllBooks= await bookeSechma.find({totalPages:{$gt:500},stockAvailable:true})
+const bookList = async function (req, res) {
+    let getAllBooks = await bookeSechma.find().select({ bookeName: 1, authorName: 1, _id: 0 })
     res.send(getAllBooks)
 }
+const getRandomBooks = async function (req, res) {
+    let getAllBooks = await bookeSechma.find({ totalPages: { $gt: 500 }, stockAvailable: true })
+    res.send(getAllBooks)
+}
+const getParticularBooks = async function (req, res) {
+    //(this is a good one, make sincere effort to solve this) take any input and use it as a condition to fetch books that satisfy that condition
+    //e.g if body had { name: “hi”} then you would fetch the books with this name
+    //if body had { year: 2020} then you would fetch the books in this year
+    //hence the condition will differ based on what you input in the request body
+    let getBookByAnyAtribute = req.body
+    let getBookByBookAnyAtribute = await bookeSechma.find(getBookByAnyAtribute)
+    res.send(getBookByBookAnyAtribute)
+}
+const getXINRBooks = async function (req, res) {
+    //request to return all books who have an Indian price tag of “100INR” or “200INR” or “500INR” 
+    //let getAllBooks= await bookeSchema.find(price:["IndianPrice"]:)
+    let getAllBooks = await bookeSechma.find({ $or: [{ "price.IndianPrice": ["Rs. 100", "Rs. 200", "Rs. 500"] }] })
+    res.send(getAllBooks)
 
-module.exports.createNewBooks = createNewBooks
-module.exports.getBookPost = getBookPost
-module.exports.getnewBook = getnewBook
+}
+module.exports.NewBooksEntry = NewBooksEntry
+module.exports.getBooksInYear = getBooksInYear
+module.exports.bookList = bookList
+module.exports.getRandomBooks = getRandomBooks
+module.exports.getParticularBooks=getParticularBooks
+module.exports.getXINRBooks = getXINRBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const books = require("../booksController/booksController")
 
-router.post("/newBooks", books.createNewBooks)
-router.post("/postApiYears",books.getBookPost)
-router.get("/getNewBooks",books.getnewBook)
+router.post("/createNewBookPost", books.NewBooksEntry)
+router.post("/postApiYears",books.getBooksInYear)
+router.get("/getAllBookList",books.bookList)
+router.get("/getAvailableBooks",books.getRandomBooks)
+router.post("/getParticularBooksApi",books.getParticularBooks)
+router.get("/getXINRBooks",books.getXINRBooks)
 
 module.exports = router;
