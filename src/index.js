db51812d15d109465f358c755da348e34ac02ba2
Kaluram <kaluramkanesh123@gmail.com>
2022-06-19 11:39:20 +0530

    19-06-2022 practice code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://kaluram123:KXluZumzdfH1m4LR@cluster0.4yhyg.mongodb.net/kaluram123-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const userController= require("../controllers/userController")
+// const userController= require("../controllers/userController")
+const userController = require("../controllers/userController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser  )
+router.post("/users", userController.createUser)
+router.post("/loginUser", userController.loginUser1)
+router.get("/getUserData/:userId",userController.userDetail)
+// router.post("/login", userController.loginUser)
+// //The userId is sent by front end
+// router.get("/users/:userId", userController.getUserData)
+// router.get("/GetUsersData/userID", userController.getUserData)
 
-router.post("/login", userController.loginUser)
-
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-
-router.put("/users/:userId", userController.updateUser)
+// router.put("/users/:userId", userController.updateUser)
 
 module.exports = router;
